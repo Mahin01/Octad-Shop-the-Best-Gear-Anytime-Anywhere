3eb32465017ede2f6a0953ec1b00e69b1c99eece
Mahin01 <ashmahin4@gmail.com>
2024-11-09 12:58:44 +0600

    Main route configured

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React, { StrictMode } from 'react'
 import './index.css'
 import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/MainRoutes';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
